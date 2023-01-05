836f9f817af99d13b08d38833ac01128c137de03
Chi Bui <chibui@users.noreply.github.com>
2023-01-05 20:49:26 +1100

    material button

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule} from '@angular/material/card';
@@ -13,7 +15,6 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class PostCreateComponent {
   enteredValue: string = '';
-  newPost: string = 'No CONTENT';
+  newPost: string = 'NO CONTENT';
 
   onAddPost() {
     this.newPost = this.enteredValue;
