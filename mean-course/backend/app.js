fc990ea551e52ae41c3ecf764daf3089d802a8fb
Chi Bui <chibui@users.noreply.github.com>
2023-01-09 11:05:40 +1100

    38. Fetching initial posts

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('First middleware');
-  next();
-});
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      content: 'This is coming from the server',
+      id: 'POST001',
+      title: 'First server-side post'
+    },
+    {
+      content: 'This is coming from the server',
+      id: 'POST002',
+      title: 'Second server-side post'
+    }
+  ];
 
-app.use((req, res, next) => {
-  res.send('Hello from express!')
+  res.status(200).json({
+    message: 'Post fetch successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
