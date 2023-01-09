08fa2f604eeac278ce724d64375cae20d34c2133
Chi Bui <chibui@users.noreply.github.com>
2023-01-09 14:41:24 +1100

    51. Understanding Mongoose Schemas & Models

---
+++ mean-course/backend/models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema({
+  content: {
+    required: true,
+    type: String
+  },
+  title: {
+    required: true,
+    type: String
+  }
+});
+
+module.exports = mongoose.model('Post', postSchema);
