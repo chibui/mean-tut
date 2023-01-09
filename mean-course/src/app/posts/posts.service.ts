127ceecdc1b9cfb538198e177bab36f5a8c4a259
Chi Bui <chibui@users.noreply.github.com>
2023-01-09 20:10:17 +1100

    56. Transforming Response Data

--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http'
-import { Subject } from "rxjs";
+import { map, Subject } from "rxjs";
+
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: 'root' })
@@ -11,9 +12,19 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+    this.http
+      .get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            content: post.content,
+            id: post._id,
+            title: post.title
+          }
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
   }
