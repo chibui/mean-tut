a7efcde3f200facc3fd97b418f00960d6a45d215
Chi Bui <chibui@users.noreply.github.com>
2023-02-20 13:45:40 +1100

    102. Implementing SPA Authentication

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-const User = require('../models/user')
+const User = require('../models/user');
+const JWT_SECRET = require('../constants');
 
 const router = express.Router();
+
+const authError = () => {
+  return res.status(401).json({
+    message: "Auth failed"
+  });
+}
 
 router.post('/signup', (req, res, next) => {
   console.log('req', req);
@@ -31,7 +39,29 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then(User => {
+      if (!User) {
+        authError();
+      }
 
+      return bcrypt.compare(req.body.password, user.password);
+    })
+    .then(result => {
+      if (!result) {
+        authError();
+      }
+
+      const token = jwt.sign({
+        email: User.email,
+        userId: User._id },
+        JWT_SECRET,
+        { expiresIn: '1h'}
+      );
+    })
+    .catch(err => {
+      authError();
+    });
 });
 
 module.exports = router;
