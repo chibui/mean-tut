06313818c8a24f9e75faef05414f7ff9f7a380d4
Chi Bui <chibui@users.noreply.github.com>
2023-02-22 14:39:21 +1100

    124. The Error Interceptor

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -22,7 +22,8 @@ import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { AuthInterceptor } from './auth/auth-interceptor';
+import { AuthInterceptor } from './auth/auth.interceptor';
+import { ErrorInterceptor } from './error.interceptor';
 
 @NgModule({
   declarations: [
@@ -50,9 +51,14 @@ import { AuthInterceptor } from './auth/auth-interceptor';
     MatProgressSpinnerModule,
     MatToolbarModule
   ],
-  providers: [{
-    provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor,
-             multi: true
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor,
+      multi: true
   }],
   bootstrap: [AppComponent]
 })
